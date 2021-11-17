8b5fb46372d08f5abf81a102cf3ebcb660429c05
xedk <digvijaysra@gmail.com>
2021-11-17 09:19:11 +0530

    total supply

--- contracts/DIGToken.sol
+++ contracts/DIGToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DIGToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Digvijay", "DIG") {
-        _mint(msg.sender, initialSupply);
+    constructor() ERC20("Digvijay", "DIG") {
+        _mint(msg.sender, 10000 * 10**18);
     }
 }
