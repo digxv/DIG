f634a2b90bfcae2434e284eb386a58fd2411d8b5
xedk <digvijaysra@gmail.com>
2021-11-17 09:37:34 +0530

    mint function

--- contracts/DIGToken.sol
+++ contracts/DIGToken.sol
@@ -3,7 +3,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DIGToken is ERC20 {
+    address public admin;
+
     constructor() ERC20("Digvijay", "DIG") {
-        _mint(msg.sender, 10000 * 10**18);
+        _mint(msg.sender, 1000000 * 10**18);
+        admin = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == admin);
+        _mint(to, amount);
     }
 }
